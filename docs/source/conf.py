a0c7b0ea27fa878a11f98c08af3147ae69d128ea
Joao-Abner <joaoabnerpereira@gmail.com>
2024-06-24 17:57:40 -0300

    Update Conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Financify Docker CRUD'
+copyright = '2024, João Abner'
+author = 'João Abner'
 
 release = '0.1'
 version = '0.1.0'
